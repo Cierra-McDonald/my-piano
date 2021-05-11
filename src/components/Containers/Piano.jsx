d21a534180e6402f7425c3c11d77c59f7b1b18a7
Cierra-McDonald <cierralauren15@gmail.com>
2021-05-11 11:00:48 -0700

    keys mapped and rendered to page

--- src/components/Containers/Piano.jsx
+++ src/components/Containers/Piano.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
 import './Piano.css'
 import Keys from '../Presentation/Keys'
+import _ from 'lodash';
+import NOTES from '../global/Notes'
 
 export default class Piano extends Component {
     render() {
+        const keys = _.map(NOTES, (note, index) => {
+            return (
+                 <Keys
+                    key={index}
+                    note={note}
+                />
+                );
+            }); 
         return (
-            <div className="piano">
-               <Keys/>
-               <Keys/>
-               <Keys/>
-            </div>
+                <div className="piano">
+                    {keys}
+                </div>
         )
     }
 }
--- src/components/Presentation/Keys.jsx
+++ src/components/Presentation/Keys.jsx
@@ -5,7 +5,6 @@ export default class Keys extends Component {
     render() {
         return (
             <div className="key">
-                <h1>Key!</h1>
             </div>
         )
     }
