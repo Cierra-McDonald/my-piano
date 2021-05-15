5b47b8ffb6f796dc8460f0cd5ed1dcd0b6d76a08
Cierra-McDonald <cierralauren15@gmail.com>
2021-05-15 12:51:54 -0700

    piano with black and white keys rendering!

--- src/components/Presentation/Keys.jsx
+++ src/components/Presentation/Keys.jsx
@@ -2,10 +2,29 @@ import React, { Component } from 'react'
 import './Keys.css'
 
 export default class Keys extends Component {
+
+    isNoteFlat = (note) => { 
+        return note.length > 1; 
+    }
+
     render() {
-        return (
-            <div className="key">
-            </div>
-        )
+        let keyClassName = "key";
+        const flatNote = this.isNoteFlat(this.props.note)
+        if(flatNote) {
+            keyClassName += " flat";  
+        }
+
+        let key;
+        if(flatNote) { 
+            key = <div className={keyClassName}></div>
+        } else{ 
+            key = (
+                <div className={keyClassName}>
+                    <div className="key-text">{this.props.note.toUpperCase()}</div>
+                </div>
+            )
+        }
+
+        return key;
     }
 }
